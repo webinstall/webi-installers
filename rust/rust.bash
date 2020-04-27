f1890991e6952feb8827c221e6bcb9ef9f3fb985
AJ ONeal <aj@therootcompany.com>
2020-04-27 04:31:19 +0000

    add rust

---
+++ rust/rust.bash
@@ -0,0 +1,6 @@
+echo ""
+echo "'rust' is ambiguous. Did you mean 'rustlang'?"
+echo ""
+echo "    curl https://webinstall.dev/rustlang | bash"
+sleep 2
+echo ""
---
+++ rustlang/rustlang.bash
@@ -0,0 +1,2 @@
+# Straight from https://rustup.rs/
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
