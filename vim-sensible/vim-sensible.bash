20006a53d7e029558055950301c4d5ea465758a6
AJ ONeal <aj@therootcompany.com>
2020-04-27 05:39:05 +0000

    add vim-sensible

---
+++ vim-sensible/vim-sensible.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# title: vim-sensible
+# homepage: https://github.com/tpope/vim-sensible
+# tagline: sensible defaults for vim
+# description: |
+#   Think of sensible.vim as one step above 'nocompatible' mode: a universal set of defaults that (hopefully) everyone can agree on.
+
+mkdir -p $HOME/.vim/pack/plugins/start
+git clone --depth=1 https://tpope.io/vim/sensible.git $HOME/.vim/pack/plugins/start/sensible
