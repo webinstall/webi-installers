ac762b60f99364184ea1ecea14a0111af9520ffa
AJ ONeal <aj@therootcompany.com>
2020-04-27 06:24:30 +0000

    cli for webinstall.dev

---
+++ webi/webi.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# title: Webi
+# homepage: https://webinstall.dev
+# tagline: webinstall.dev for the CLI
+# description: |
+#   for the people like us that are too lazy even to run `curl https://webinstall.dev/PACKAGE_NAME` - just `webi PACKAGE_NAME` instead
+
+set -e
+set -u
+
+my_package=${1:-}
+if [ -z "$my_package" ]; then
+	echo "Usage: webi <package>"
+	echo "Example: webi node"
+	exit 1
+fi
+
+curl -fsSL "https://webinstall.dev/$my_package" | bash
