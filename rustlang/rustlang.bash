40c68aff32a899150625e3c0847410dccaf6e3fa
AJ ONeal <aj@therootcompany.com>
2020-04-27 05:30:46 +0000

    add meta for rustlang

--- rustlang/rustlang.bash
+++ rustlang/rustlang.bash
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# title: Rust
+# homepage: https://rust-lang.org
+# tagline: The Rust Toolchain
+# description: |
+#   A language empowering everyone to build reliable and efficient software.
+
 # Straight from https://rustup.rs/
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
