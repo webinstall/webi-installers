f54b126b23063d0ce94693d2a00363bd12bb4d72
AJ ONeal <aj@therootcompany.com>
2020-04-27 05:39:17 +0000

    add meta for brew

--- brew/brew.bash
+++ brew/brew.bash
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# title: Homebrew
+# homepage: https://brew.sh
+# tagline: The Missing Package Manager for macOS (or Linux)
+# description: |
+#   Homebrew installs the stuff you need that Apple (or your Linux system) didn’t.
+
 # Straight from https://brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
