a3fd85298aa7704041238bbaf6f9426fe5c47f68
AJ ONeal <aj@therootcompany.com>
2020-05-03 10:52:48 +0000

    add default arch amd64

--- pathman/releases.js
+++ pathman/releases.js
@@ -7,6 +7,12 @@ var baseurl = 'https://git.coolaj86.com';
 
 module.exports = function (request) {
   return github(request, owner, repo, baseurl).then(function (all) {
+    all.releases.forEach(function (rel) {
+      // TODO name uploads with arch, duh
+      if (!rel.arch) {
+        rel.arch = 'amd64';
+      }
+    });
     return all;
   });
 };
