13d2ca88fc57c237ab79e8a679c38db75e04e088
AJ ONeal <coolaj86@gmail.com>
2020-06-13 16:06:28 -0600

    update PATH so that webi can work

--- webi/install.bash
+++ webi/install.bash
@@ -21,7 +21,14 @@
 #   ```
 
 if [ -f "$HOME/.local/bin/webi" ]; then
+  set +e
+  cur_webi="$(command -v webi)"
+  set -e
+  if [ -z "$cur_webi" ]; then
+    web_add_path "$HOME/.local/bin"
+  fi
   echo "Installed 'webi'"
 else
+  # for when this file is run on its own, not from webinstall.dev
   echo "Install any other package via https://webinstall.dev and webi will be installed as part of the bootstrap process"
 fi
