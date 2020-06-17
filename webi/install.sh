2a63b1c6f82da905a62c4a0fad85a7cd0adca7da
AJ ONeal <aj@therootcompany.com>
2020-06-17 06:43:50 +0000

    add standard files

--- webi/install.sh
+++ webi/install.sh
@@ -1,49 +1,7 @@
 #!/bin/bash
 
-# title: Webi
-# homepage: https://webinstall.dev
-# tagline: |
-#   Webi is how developers install their tools.
-# description: |
-#   Webi is what you would have created if you automated how you install your common tools yourself: Simple, direct downloads from official sources, unpacked into `$HOME/.local`, added to `PATH`, symlinked for easy version switching, with minimal niceties like resuming downloads and 'stable' tags.
-#   
-#   - Easy to remember.
-#   - No magic, no nonesense, no bulk.
-#   - What you would have done for yourself.
-#
-# examples: |
-#   You can install _exactly_ what you need, from memory, via URL:
-#
-#   ```bash
-#   curl https://webinstall.dev/node@lts | bash
-#   ```
-#
-#   Or via `webi`, the tiny `curl | bash` shortcut command that comes with each install:
-#
-#   ```bash
-#   webi node@latest
-#   ```
-#
-#   ```bash
-#   webi golang@v1.14
-#   ```
-#
-#   ```bash
-#   webi rustlang
-#   ```
-#
-#   You can see exactly what PATHs have been edited:
-#
-#   ```bash
-#   pathman list
-#   ```
-#
-#   And where:
-#
-#   ```bash
-#   cat $HOME/.config/envman/PATH.env
-#   ```
-#
+# Note: 'webi' is a special case. It's actually just a helper utility that comes with every installer.
+#       See https://github.com/webinstall/packages/blob/master/_webi/bootstrap.sh for the source.
 
 {
 
