65ebe70d56028eb2f6c7017a72bb8384eda28523
AJ ONeal <coolaj86@gmail.com>
2020-07-01 00:59:20 -0600

    add pwsh as alias for powershell

---
+++ pwsh/install.sh
@@ -0,0 +1,10 @@
+# title: PowerShell (pwsh alias)
+# homepage: https://webinstall.dev/powershell
+# tagline: Alias for https://webinstall.dev/powershell
+# alias: powershell
+# description: |
+#   See https://webinstall.dev/powershell
+
+echo "'pwsh@${WEBI_TAG:-stable}' is an alias for 'powershell@${WEBI_VERSION:-}'"
+WEBI_HOST=${WEBI_HOST:-"https://webinstall.dev"}
+curl -fsSL "$WEBI_HOST/powershell@${WEBI_VERSION:-}" | bash
