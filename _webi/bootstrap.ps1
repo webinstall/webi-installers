1b521fc4079e7f0b4ba0821757d6e3f3c2bbbd49
Jon Kunkee <jonathan.kunkee@gmail.com>
2020-07-04 15:53:41 -0700

    Simplify boostrap.ps1

--- _webi/bootstrap.ps1
+++ _webi/bootstrap.ps1
@@ -2,6 +2,5 @@
 IF (!(Test-Path -Path .local\bin)) { New-Item -Path .local\bin -ItemType Directory }
 IF ($Env:WEBI_HOST -eq $null -or $Env:WEBI_HOST -eq "") { $Env:WEBI_HOST = "https://webinstall.dev" }
 curl.exe -s -A "windows" "$Env:WEBI_HOST/packages/_webi/webi.ps1" -o "$Env:USERPROFILE\.local\bin\webi.ps1"
-pushd "$Env:USERPROFILE"
-& powershell -ExecutionPolicy Bypass ".local\bin\webi.ps1" "{{ exename }}"
-popd
+Set-ExecutionPolicy -Scope Process Bypass
+& "$Env:USERPROFILE\a b\webi.ps1" "{{ exename }}"
