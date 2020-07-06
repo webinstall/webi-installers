a553740db55b30e1a7cc13f8fed224ee9e582e32
AJ ONeal <aj@therootcompany.com>
2020-07-06 04:15:25 +0000

    minor fixes

--- vps-addswap/install.sh
+++ vps-addswap/install.sh
@@ -7,6 +7,16 @@
     default_size=2G
     my_size=${1:-$default_size}
 
+    if [ "0" != "$EUID" ] && [ "0" != "$UID" ]; then
+      echo Usage:
+      echo '        sudo env PATH="$PATH" vps-addswap' $my_size
+      exit 1
+    fi
+
+    if [ -f "/var/swapfile" ]; then
+      swapoff /var/swapfile
+    fi
+
     # Allocate a swapfile
     fallocate -l "$my_size" /var/swapfile
 
@@ -20,5 +30,4 @@
 
     # Cause swap to be activated on boot
     echo '/var/swapfile none swap sw 0 0' | tee -a /etc/fstab
-
 }
