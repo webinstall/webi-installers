c79bace5060864dcbd0d4202fcefd1fa667218c4
SenorGrande <clooneyjh@gmail.com>
2020-10-04 16:11:14 +1300

    add goreleaser

---
+++ goreleaser/README.md
@@ -0,0 +1,31 @@
+---
+title: goreleaser
+homepage: https://goreleaser.com
+tagline: |
+  goreleaser: Deliver Go binaries as fast and easily as possible
+---
+
+### Updating `goreleaser`
+
+`webi goreleaser@stable`
+
+Use the `@beta` tag for pre-releases.
+
+## Cheat Sheet
+
+> `goreleaser` doesn't exist and this text should have been replaced. It doesn't do
+> anything, but what it does is useful because it is; everybody knows it.
+
+To run goreleaser:
+
+```bash
+goreleaser
+```
+
+### Add Baz Highlighting
+
+To run goreleaser with both bar and baz highlighting turned on:
+
+```bash
+goreleaser --bar=baz
+```
---
+++ goreleaser/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+{
+    set -e
+    set -u
+
+    ######################
+    # Install goreleaser #
+    ######################
+
+    # Every package should define these 6 variables
+    pkg_cmd_name="goreleaser"
+
+    pkg_dst_cmd="$HOME/.local/bin/goreleaser"
+    pkg_dst="$pkg_dst_cmd"
+
+    pkg_src_cmd="$HOME/.local/opt/goreleaser-v$WEBI_VERSION/bin/goreleaser"
+    pkg_src_dir="$HOME/.local/opt/goreleaser-v$WEBI_VERSION"
+    pkg_src="$pkg_src_cmd"
+
+    # pkg_install must be defined by every package
+    pkg_install() {
+        # ~/.local/opt/goreleaser-v0.99.9/bin
+        mkdir -p "$(dirname $pkg_src_cmd)"
+
+        # mv ./goreleaser-*/goreleaser ~/.local/opt/goreleaser-v0.99.9/bin/goreleaser
+        mv ./goreleaser "$pkg_src_cmd"
+    }
+
+    # pkg_get_current_version is recommended, but (soon) not required
+    pkg_get_current_version() {
+        # 'goreleaser --version' has output in this format:
+        #       goreleaser 0.99.9 (rev abcdef0123)
+        # This trims it down to just the version number:
+        #       0.99.9
+        echo $(goreleaser --version 2>/dev/null | head -n 1 | cut -d ' ' -f 2)
+    }
+
+}
