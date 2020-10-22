2f624b99059c65bb327aa47a8ae3682b97e15c43
AJ ONeal <aj@therootcompany.com>
2020-10-22 08:54:30 +0000

    add iterm as alias to iterm2

---
+++ iterm/install.sh
@@ -0,0 +1,10 @@
+# title: iTerm (iTerm2 alias)
+# homepage: https://webinstall.dev/iterm2
+# tagline: Alias for https://webinstall.dev/iterm2
+# alias: iterm2
+# description: |
+#   See https://webinstall.dev/iterm2
+
+echo "'iterm@${WEBI_TAG:-stable}' is an alias for 'iterm2@${WEBI_VERSION:-}'"
+WEBI_HOST=${WEBI_HOST:-"https://webinstall.dev"}
+curl -fsSL "$WEBI_HOST/iterm2@${WEBI_VERSION:-}" | bash
