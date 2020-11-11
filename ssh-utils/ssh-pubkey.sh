c25c0354ea27568a04315b820d0ccd26bdea0264
AJ ONeal <coolaj86@gmail.com>
2020-11-11 13:37:26 -0700

    print to stderr (debug) except public key

--- ssh-utils/ssh-pubkey.sh
+++ ssh-utils/ssh-pubkey.sh
@@ -22,20 +22,20 @@
 
     if [ ! -f "$HOME/.ssh/id_rsa" ]; then
         ssh-keygen -b 2048 -t rsa -f "$HOME/.ssh/id_rsa" -q -N ""
-        echo ""
+        >&2 echo ""
     fi
 
     if [ ! -f "$HOME/.ssh/id_rsa.pub" ]; then
         ssh-keygen -y -f "$HOME/.ssh/id_rsa" > "$HOME/.ssh/id_rsa.pub"
-        echo ""
+        >&2 echo ""
     fi
 
     # TODO use the comment (if any) for the name of the file
-    echo ""
-    echo "~/Downloads/id_rsa.$(whoami).pub":
-    echo ""
-    rm -f "$HOME/Downloads/id_rsa.$(whoami).pub":
+    >&2 echo ""
+    >&2 echo "~/Downloads/id_rsa.$(whoami).pub":
+    >&2 echo ""
+    rm -f "$HOME/Downloads/id_rsa.$(whoami).pub"
     cp -r "$HOME/.ssh/id_rsa.pub" "$HOME/Downloads/id_rsa.$(whoami).pub"
     cat "$HOME/Downloads/id_rsa.$(whoami).pub"
-    echo ""
+    >&2 echo ""
 }
