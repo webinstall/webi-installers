2220875889faa800a9673d39b3e18a352399c94a
Adithya Sunil Edakkadan <adithyasunil26@gmail.com>
2020-12-31 00:46:44 +0400

    add yq

---
+++ yq/README.md
@@ -0,0 +1,8 @@
+---
+title: yq
+homepage: https://github.com/mikefarah/yq
+tagline: |
+  yq: a lightweight and portable command-line YAML processor
+
+---
+
---
+++ yq/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+{
+    set -e
+    set -u
+
+    pkg_cmd_name="yq"
+
+    pkg_dst_cmd="$HOME/.local/bin/yq"
+    pkg_dst="$pkg_dst_cmd"
+
+    pkg_src_cmd="$HOME/.local/opt/yq-v$WEBI_VERSION/bin/yq"
+    pkg_src_dir="$HOME/.local/opt/yq-v$WEBI_VERSION"
+    pkg_src="$pkg_src_cmd"
+
+    pkg_install() {
+        mkdir -p "$(dirname $pkg_src_cmd)"
+        mv ./"$pkg_cmd_name"* "$pkg_src_cmd"
+        chmod a+x "$pkg_src_cmd"
+    }
+
+    pkg_get_current_version() {
+        echo $(yq --version 2>/dev/null | head -n 1 | cut -d ' ' -f 2)
+    }
+
+}
