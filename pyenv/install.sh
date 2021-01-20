6b0212a48036df1bd4aef1a6dc0f17aa85c3612d
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-20 11:34:49 +0400

    replaced if statements with pathman

--- pyenv/install.sh
+++ pyenv/install.sh
@@ -2,13 +2,6 @@
 
 {
     curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-    if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-        echo 'export PATH="$HOME/.pyenv/bin:$PATH"'>> ~/.zshrc
-        echo 'eval "$(pyenv init -)"'>> ~/.zshrc
-        echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
-    else
-        echo 'export PATH="$HOME/.pyenv/bin:$PATH"'>> ~/.bashrc
-        echo 'eval "$(pyenv init -)"'>> ~/.bashrc
-        echo 'eval "$(pyenv virtualenv-init -)"'>> ~/.bashrc
-    fi
+    pathman add ~/.pyenv
+    pathman add ~/.pyenv/shim    
 }
