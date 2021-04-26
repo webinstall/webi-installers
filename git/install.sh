5be42d10d326b6018e9f27da6384db607ef10811
AJ ONeal <aj@therootcompany.com>
2021-04-26 08:15:35 +0000

    bugfix: git installer: don't invoke install function with ()

--- git/install.sh
+++ git/install.sh
@@ -6,12 +6,12 @@ function __init_git() {
 
     if [ -z "$(command -v git)" ]; then
         if [[ -n "$(uname -a | grep -i darwin)" ]]; then
-            >&2 echo "Error: 'git' not found. You may have to re-install 'git' on Mac after every major update."
-            >&2 echo "       for example, try: xcode-select --install"
+            echo >&2 "Error: 'git' not found. You may have to re-install 'git' on Mac after every major update."
+            echo >&2 "       for example, try: xcode-select --install"
             # sudo xcodebuild -license accept
         else
-            >&2 echo "Error: to install 'git' on Linux use the built-in package manager."
-            >&2 echo "       for example, try: xcode-select --install"
+            echo >&2 "Error: to install 'git' on Linux use the built-in package manager."
+            echo >&2 "       for example, try: xcode-select --install"
         fi
         exit 1
     else
@@ -20,4 +20,4 @@ function __init_git() {
 
 }
 
-__init_git()
+__init_git
