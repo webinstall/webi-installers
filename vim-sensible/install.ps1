81f9a11500b27b6e3006a05d202d4f287104b95d
AJ ONeal <coolaj86@gmail.com>
2021-11-05 04:21:49 -0600

    bugfix(vim-sensible): use github url due to issues with some git versions not following redirect

--- vim-sensible/install.ps1
+++ vim-sensible/install.ps1
@@ -4,4 +4,10 @@ IF (!(Test-Path -Path "$Env:USERPROFILE\.vim\pack\plugins\start")) {
     New-Item -Path "$Env:USERPROFILE\.vim\pack\plugins\start" -ItemType Directory -Force | out-null
 }
 Remove-Item -Path "$Env:USERPROFILE\.vim\pack\plugins\start\vim-sensible" -Recurse -ErrorAction Ignore
-& git clone --depth=1 https://tpope.io/vim/sensible.git "$Env:USERPROFILE\.vim\pack\plugins\start\vim-sensible"
+
+# Note: we've had resolution issues in the past, and it doesn't seem likely that tpope
+#       will switch from using GitHub as the primary host, so we skip the redirect
+#       and use GitHub directly. Open to changing this back in the future.
+#$sensible_repo = "https://tpope.io/vim/sensible.git"
+$sensible_repo = "https://github.com/tpope/vim-sensible.git"
+& git clone --depth=1 "$sensible_repo" "$Env:USERPROFILE\.vim\pack\plugins\start\vim-sensible"
--- vim-sensible/install.sh
+++ vim-sensible/install.sh
@@ -6,7 +6,14 @@ function __init_vim_sensible() {
 
     mkdir -p "$HOME/.vim/pack/plugins/start"
     rm -rf "$HOME/.vim/pack/plugins/start/sensible" "$HOME/.vim/pack/plugins/start/vim-sensible"
-    git clone --depth=1 https://tpope.io/vim/sensible.git "$HOME/.vim/pack/plugins/start/vim-sensible"
+
+    # Note: we've had resolution issues in the past, and it doesn't seem likely
+    #       that tpope will switch from using GitHub as the primary host, so we
+    #       skip the redirect and use GitHub directly.
+    #       Open to changing this back in the future.
+    #my_sensible_repo="https://tpope.io/vim/sensible.git"
+    my_sensible_repo="https://github.com/tpope/vim-sensible.git"
+    git clone --depth=1 "${my_sensible_repo}" "$HOME/.vim/pack/plugins/start/vim-sensible"
 }
 
 __init_vim_sensible
