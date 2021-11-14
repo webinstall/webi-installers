7e22ad9b79497fd4e0f54945ad53e496c03b1b4a
AJ ONeal <aj@therootcompany.com>
2021-11-14 09:39:12 +0000

    chore(rustlang): bash style update (use function)

--- rustlang/install.sh
+++ rustlang/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# Straight from https://rustup.rs/
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+function __install_rust() {
+    # Straight from https://rustup.rs/
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+__install_rust
