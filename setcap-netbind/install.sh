85a5689dbb79c3c0bead61586c294cfd1585a1d1
AJ ONeal <aj@therootcompany.com>
2021-11-21 05:32:10 +0000

    refactor(setcap-netbind): update bash to style, update docs

--- setcap-netbind/install.sh
+++ setcap-netbind/install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+set -e
+set -u
 
-{
-    set -e
-    set -u
+function __install_setcap_netbind() {
+    # remove prior version, if exists
+    rm -f ~/.local/bin/setcap-netbind
 
-    rm -f "$HOME/.local/bin/setcap-netbind"
-    webi_download "$WEBI_HOST/packages/setcap-netbind/setcap-netbind.sh" "$HOME/.local/bin/setcap-netbind"
-    chmod a+x "$HOME/.local/bin/setcap-netbind"
+    # download latest version, directly to ~/.local/bin
+    webi_download \
+        "$WEBI_HOST/packages/setcap-netbind/setcap-netbind.sh" \
+        ~/.local/bin/setcap-netbind
+
+    # make executable
+    chmod a+x ~/.local/bin/setcap-netbind
 }
+
+__install_setcap_netbind
