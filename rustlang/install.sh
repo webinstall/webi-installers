ee594b4985f9af09873200872f33ba881aa97431
Marcin Woźniak <y0rune@aol.com>
2022-03-28 11:40:25 +0200

    rustlang: Added the pathman, disabled SC2016

--- rustlang/install.sh
+++ rustlang/install.sh
@@ -2,9 +2,14 @@
 set -e
 set -u
 
+# shellcheck disable=SC2016
+
 function __install_rust() {
+
     # Straight from https://rustup.rs/
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+    pathman add "$HOME/.cargo/bin"
 }
 
 __install_rust
