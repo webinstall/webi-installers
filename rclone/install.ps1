f5843b85199fc3f64a576d9f686d4801486b6ad9
Tristan Engel <121832013+trengel@users.noreply.github.com>
2023-09-06 12:57:22 -0400

    fix(rclone): update install.ps1 (fix #636)

--- rclone/install.ps1
+++ rclone/install.ps1
@@ -46,7 +46,7 @@ IF (!(Test-Path -Path "$pkg_src_cmd"))
         # Settle unpacked archive into place
         echo "Install Location: $pkg_src_cmd"
         New-Item "$pkg_src_bin" -ItemType Directory -Force | out-null
-        Move-Item -Path ".\rclone.exe" -Destination "$pkg_src_bin"
+        Move-Item -Path ".\rclone-v*\rclone.exe" -Destination "$pkg_src_bin"
 
     # Exit tmp
     popd
