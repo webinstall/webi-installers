aa616be91772364e97c80a9bbcc94783df9536e7
AJ ONeal <aj@therootcompany.com>
2023-10-16 19:56:45 +0000

    ref(terraform): oppa AJ style

--- terraform/install.sh
+++ terraform/install.sh
@@ -7,19 +7,25 @@ pkg_cmd_name="terraform"
 WEBI_SINGLE=true
 
 pkg_get_current_version() {
+    # 'terraform -v' has output in this format:
+    #       Terraform v1.6.1
+    #       on linux_amd64
+    # This trims it down to just the version number:
+    #       1.6.1
     terraform -v 2> /dev/null |
         head -n 1 |
-        cut -d 'v' -f 2
+        cut -d' ' -f2 |
+        cut -c2-
 }
 
 pkg_install() {
-    # $HOME/.local/bin/opt/terraform-v1.3.2/bin
+    # $HOME/.local/bin/opt/terraform-v1.6.1/bin
     mkdir -p "$pkg_src_bin"
 
-    # mv ./terraform* "$HOME/.local/opt/terraform-v1.3.2/bin/terraform"
+    # mv ./terraform* "$HOME/.local/opt/terraform-v1.6.1/bin/terraform"
     mv ./"$pkg_cmd_name"* "$pkg_src_cmd"
 
-    # chmod a+x "$HOME/.local/opt/terraform-v1.3.2/bin/terraform"
+    # chmod a+x "$HOME/.local/opt/terraform-v1.6.1/bin/terraform"
     chmod a+x "$pkg_src_cmd"
 }
 
@@ -27,6 +33,6 @@ pkg_link() {
     # rm -f "$HOME/.local/bin/terraform"
     rm -f "$pkg_dst_cmd"
 
-    # ln -s "$HOME/.local/opt/terraform-v1.3.2/bin/terraform" "$HOME/.local/bin/terraform"
+    # ln -s "$HOME/.local/opt/terraform-v1.6.1/bin/terraform" "$HOME/.local/bin/terraform"
     ln -s "$pkg_src_cmd" "$pkg_dst_cmd"
 }
