b3e2f0c2d177c2e41011f6165ad68dbc453da3e5
AJ ONeal <aj@therootcompany.com>
2023-10-28 16:53:26 -0600

    workaround(ollama): filter out v0.0.x releases that are sorting too high

--- ollama/releases.js
+++ ollama/releases.js
@@ -4,13 +4,22 @@ var github = require('../_common/github.js');
 var owner = 'jmorganca';
 var repo = 'ollama';
 
-module.exports = function (request) {
-  return github(request, owner, repo).then(function (all) {
-    all.releases.forEach(function (rel) {
-      rel.version = String(rel.version).replace(/^v/, '');
-    });
-    return all;
-  });
+module.exports = async function (request) {
+  let all = await github(request, owner, repo);
+
+  // TODO why are the 0.0.x releases sorting so high?
+  let releases = [];
+  for (let rel of all.releases) {
+    let isLow = rel.version.startsWith('v0.0.');
+    if (isLow) {
+      continue;
+    }
+
+    releases.push(rel);
+  }
+  all.releases = releases;
+
+  return all;
 };
 
 if (module === require.main) {
