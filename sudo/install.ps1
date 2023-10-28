30f161f032a0ca80e77506c92369ad0263ce8862
AJ ONeal <aj@therootcompany.com>
2023-10-28 07:15:54 +0000

    fix(sudo): always create sudo.cmd in Windows user's home directory

--- sudo/install.ps1
+++ sudo/install.ps1
@@ -3,7 +3,7 @@
 Write-Output "Installing sudo.cmd..."
 
 # Couldn't figure out how to get this to work with "here strings", so forgive the ugly, but at least it works
-Set-Content -Path .local\bin\sudo.cmd -Value "@echo off`r`npowershell -Command ""Start-Process cmd -Verb RunAs -ArgumentList '/c cd /d %CD% && %*'""`r`n@echo on"
+Set-Content -Path "$Env:USERPROFILE\.local\bin\sudo.cmd" -Value "@echo off`r`npowershell -Command ""Start-Process cmd -Verb RunAs -ArgumentList '/c cd /d %CD% && %*'""`r`n@echo on"
 
 Write-Output "Installed to '$Env:USERPROFILE\.local\bin\sudo.cmd'"
 Write-Output ""
