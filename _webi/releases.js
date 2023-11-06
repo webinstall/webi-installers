53d023aac9c8c7c2d949e80fe08f11b7e36d5f91
AJ ONeal <aj@therootcompany.com>
2023-11-06 21:47:19 +0000

    ref(api): use await to de-indent some async functions

--- _webi/releases.js
+++ _webi/releases.js
@@ -38,132 +38,120 @@ Releases.renderBash = async function (
   if (!tag) {
     tag = '';
   }
-  return fs.promises
-    .readFile(path.join(pkgdir, 'install.sh'), 'utf8')
-    .then(function (installTxt) {
-      installTxt = padScript(installTxt);
-      var vers = rel.version.split('.');
-      var v = {
-        major: vers.shift() || '',
-        minor: vers.shift() || '',
-        patch: vers.join('.').replace(/[+\-].*/, ''),
-        build: vers
-          .join('.')
-          .replace(/[^+\-]*/, '')
-          .replace(/^-/, ''),
-      };
-      var pkgFile = rel.filename || rel.name;
-      return fs.promises
-        .readFile(path.join(__dirname, 'template.sh'), 'utf8')
-        .then(function (tplTxt) {
-          // ex: 'node@lts' or 'node'
-          var webiPkg = pkg;
-          if (ver) {
-            webiPkg += `@${ver}`;
-          }
-          return (
-            tplTxt
-              .replace(/CHEATSHEET_URL/g, `${baseurl}/${pkg}`)
-              .replace(/^\s*#?WEBI_PKG=.*/m, `WEBI_PKG='${webiPkg}'`)
-              .replace(/^\s*#?WEBI_HOST=.*/m, `WEBI_HOST='${baseurl}'`)
-              .replace(/^\s*#?WEBI_OS=.*/m, `WEBI_OS='${os}'`)
-              .replace(/^\s*#?WEBI_ARCH=.*/m, `WEBI_ARCH='${arch}'`)
-              .replace(/^\s*#?WEBI_TAG=.*/m, `WEBI_TAG='${tag}'`)
-              .replace(
-                /^\s*#?WEBI_RELEASES=.*/m,
-                "WEBI_RELEASES='" +
-                  baseurl +
-                  '/api/releases/' +
-                  pkg +
-                  '@' +
-                  tag +
-                  '.tab?os=' +
-                  rel.os +
-                  '&arch=' +
-                  rel.arch +
-                  '&formats=' +
-                  formats.join(',') +
-                  '&pretty=true' +
-                  "'",
-              )
-              .replace(
-                /^\s*#?WEBI_CSV=.*/m,
-                "WEBI_CSV='" +
-                  [
-                    rel.version,
-                    rel.lts,
-                    rel.channel,
-                    rel.date,
-                    rel.os,
-                    rel.arch,
-                    rel.ext,
-                    '-',
-                    rel.download,
-                    rel.name,
-                    rel.comment || '',
-                  ]
-                    .join(',')
-                    .replace(/'/g, '') +
-                  "'",
-              )
-              .replace(
-                /^\s*#?WEBI_VERSION=.*/m,
-                "WEBI_VERSION='" + rel.version + "'",
-              )
-              .replace(/^\s*#?WEBI_MAJOR=.*/m, 'WEBI_MAJOR=' + v.major)
-              .replace(/^\s*#?WEBI_MINOR=.*/m, 'WEBI_MINOR=' + v.minor)
-              .replace(/^\s*#?WEBI_PATCH=.*/m, 'WEBI_PATCH=' + v.patch)
-              .replace(/^\s*#?WEBI_BUILD=.*/m, 'WEBI_BUILD=' + v.build)
-              .replace(
-                /^\s*#?WEBI_GIT_TAG=.*/m,
-                "WEBI_GIT_TAG='" + rel.git_tag + "'",
-              )
-              .replace(/^\s*#?WEBI_LTS=.*/m, 'WEBI_LTS=' + rel.lts)
-              .replace(/^\s*#?WEBI_CHANNEL=.*/m, 'WEBI_CHANNEL=' + rel.channel)
-              .replace(
-                /^\s*#?WEBI_EXT=.*/m,
-                'WEBI_EXT=' + rel.ext.replace(/tar.*/, 'tar'),
-              )
-              .replace(
-                /^\s*#?WEBI_FORMATS=.*/m,
-                "WEBI_FORMATS='" + formats.join(',') + "'",
-              )
-              .replace(
-                /^\s*#?WEBI_PKG_URL=.*/m,
-                "WEBI_PKG_URL='" + rel.download + "'",
-              )
-              // TODO replace WEBI_PKG_FILE (which is sometimes a dir)
-              .replace(
-                /^\s*#?WEBI_PKG_PATHNAME=.*/m,
-                "WEBI_PKG_PATHNAME='" + pkgFile + "'",
-              )
-              // TODO deprecate
-              .replace(
-                /^\s*#?WEBI_PKG_FILE=.*/m,
-                "WEBI_PKG_FILE='" + pkgFile + "'",
-              )
-              // PKG details
-              .replace(/^\s*#?PKG_NAME=.*/m, "PKG_NAME='" + pkg + "'")
-              .replace(
-                /^\s*#?PKG_OSES=.*/m,
-                "PKG_OSES='" + ((rel && rel.oses) || []).join(',') + "'",
-              )
-              .replace(
-                /^\s*#?PKG_ARCHES=.*/m,
-                "PKG_ARCHES='" + ((rel && rel.arches) || []).join(',') + "'",
-              )
-              .replace(
-                /^\s*#?PKG_FORMATS=.*/m,
-                "PKG_FORMATS='" + ((rel && rel.formats) || []).join(',') + "'",
-              )
-              // $', $0, ... $9, $`, $&, and $_ all have special meaning
-              // (see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/RegExp)
-              // However, it can be escaped with $$ (which must be escaped with $$)
+  let installTxt = await fs.promises.readFile(
+    path.join(pkgdir, 'install.sh'),
+    'utf8',
+  );
+  installTxt = padScript(installTxt);
+  var vers = rel.version.split('.');
+  var v = {
+    major: vers.shift() || '',
+    minor: vers.shift() || '',
+    patch: vers.join('.').replace(/[+\-].*/, ''),
+    build: vers
+      .join('.')
+      .replace(/[^+\-]*/, '')
+      .replace(/^-/, ''),
+  };
+  var pkgFile = rel.filename || rel.name;
+  let tplTxt = await fs.promises.readFile(
+    path.join(__dirname, 'template.sh'),
+    'utf8',
+  );
+  // ex: 'node@lts' or 'node'
+  var webiPkg = pkg;
+  if (ver) {
+    webiPkg += `@${ver}`;
+  }
+  return (
+    tplTxt
+      .replace(/CHEATSHEET_URL/g, `${baseurl}/${pkg}`)
+      .replace(/^\s*#?WEBI_PKG=.*/m, `WEBI_PKG='${webiPkg}'`)
+      .replace(/^\s*#?WEBI_HOST=.*/m, `WEBI_HOST='${baseurl}'`)
+      .replace(/^\s*#?WEBI_OS=.*/m, `WEBI_OS='${os}'`)
+      .replace(/^\s*#?WEBI_ARCH=.*/m, `WEBI_ARCH='${arch}'`)
+      .replace(/^\s*#?WEBI_TAG=.*/m, `WEBI_TAG='${tag}'`)
+      .replace(
+        /^\s*#?WEBI_RELEASES=.*/m,
+        "WEBI_RELEASES='" +
+          baseurl +
+          '/api/releases/' +
+          pkg +
+          '@' +
+          tag +
+          '.tab?os=' +
+          rel.os +
+          '&arch=' +
+          rel.arch +
+          '&formats=' +
+          formats.join(',') +
+          '&pretty=true' +
+          "'",
+      )
+      .replace(
+        /^\s*#?WEBI_CSV=.*/m,
+        "WEBI_CSV='" +
+          [
+            rel.version,
+            rel.lts,
+            rel.channel,
+            rel.date,
+            rel.os,
+            rel.arch,
+            rel.ext,
+            '-',
+            rel.download,
+            rel.name,
+            rel.comment || '',
+          ]
+            .join(',')
+            .replace(/'/g, '') +
+          "'",
+      )
+      .replace(/^\s*#?WEBI_VERSION=.*/m, "WEBI_VERSION='" + rel.version + "'")
+      .replace(/^\s*#?WEBI_MAJOR=.*/m, 'WEBI_MAJOR=' + v.major)
+      .replace(/^\s*#?WEBI_MINOR=.*/m, 'WEBI_MINOR=' + v.minor)
+      .replace(/^\s*#?WEBI_PATCH=.*/m, 'WEBI_PATCH=' + v.patch)
+      .replace(/^\s*#?WEBI_BUILD=.*/m, 'WEBI_BUILD=' + v.build)
+      .replace(/^\s*#?WEBI_GIT_TAG=.*/m, "WEBI_GIT_TAG='" + rel.git_tag + "'")
+      .replace(/^\s*#?WEBI_LTS=.*/m, 'WEBI_LTS=' + rel.lts)
+      .replace(/^\s*#?WEBI_CHANNEL=.*/m, 'WEBI_CHANNEL=' + rel.channel)
+      .replace(
+        /^\s*#?WEBI_EXT=.*/m,
+        'WEBI_EXT=' + rel.ext.replace(/tar.*/, 'tar'),
+      )
+      .replace(
+        /^\s*#?WEBI_FORMATS=.*/m,
+        "WEBI_FORMATS='" + formats.join(',') + "'",
+      )
+      .replace(/^\s*#?WEBI_PKG_URL=.*/m, "WEBI_PKG_URL='" + rel.download + "'")
+      // TODO replace WEBI_PKG_FILE (which is sometimes a dir)
+      .replace(
+        /^\s*#?WEBI_PKG_PATHNAME=.*/m,
+        "WEBI_PKG_PATHNAME='" + pkgFile + "'",
+      )
+      // TODO deprecate
+      .replace(/^\s*#?WEBI_PKG_FILE=.*/m, "WEBI_PKG_FILE='" + pkgFile + "'")
+      // PKG details
+      .replace(/^\s*#?PKG_NAME=.*/m, "PKG_NAME='" + pkg + "'")
+      .replace(
+        /^\s*#?PKG_OSES=.*/m,
+        "PKG_OSES='" + ((rel && rel.oses) || []).join(',') + "'",
+      )
+      .replace(
+        /^\s*#?PKG_ARCHES=.*/m,
+        "PKG_ARCHES='" + ((rel && rel.arches) || []).join(',') + "'",
+      )
+      .replace(
+        /^\s*#?PKG_FORMATS=.*/m,
+        "PKG_FORMATS='" + ((rel && rel.formats) || []).join(',') + "'",
+      )
+      // $', $0, ... $9, $`, $&, and $_ all have special meaning
+      // (see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/RegExp)
+      // However, it can be escaped with $$ (which must be escaped with $$)
 
-              .replace(reInstallTpl, '\n' + installTxt.replace(/\$/g, '$$$$'))
-          );
-        });
-    });
+      .replace(reInstallTpl, '\n' + installTxt.replace(/\$/g, '$$$$'))
+  );
 };
 
 Releases.renderPowerShell = async function (
@@ -177,64 +165,61 @@ Releases.renderPowerShell = async function (
   if (!tag) {
     tag = '';
   }
-  return fs.promises
-    .readFile(path.join(pkgdir, 'install.ps1'), 'utf8')
-    .then(function (installTxt) {
-      installTxt = padScript(installTxt);
-      /*
-      var vers = rel.version.split('.');
-      var v = {
-        major: vers.shift() || '',
-        minor: vers.shift() || '',
-        patch: vers.join('.').replace(/[+\-].*$/, ''),
-        build: vers
-          .join('.')
-          .replace(/[^+\-]*()/, '')
-          .replace(/^-/, '')
-      };
-      */
-      return fs.promises
-        .readFile(path.join(__dirname, 'template.ps1'), 'utf8')
-        .then(function (tplTxt) {
-          var pkgver = pkg + '@' + ver;
-          return (
-            tplTxt
-              .replace(
-                /^(#)?\$Env:WEBI_HOST\s*=.*/im,
-                "$Env:WEBI_HOST = '" + baseurl + "'",
-              )
-              .replace(
-                /^(#)?\$Env:WEBI_PKG\s*=.*/im,
-                "$Env:WEBI_PKG = '" + pkgver + "'",
-              )
-              .replace(
-                /^(#)?\$Env:PKG_NAME\s*=.*/im,
-                "$Env:PKG_NAME = '" + pkg + "'",
-              )
-              .replace(
-                /^(#)?\$Env:WEBI_VERSION\s*=.*/im,
-                "$Env:WEBI_VERSION = '" + rel.version + "'",
-              )
-              .replace(
-                /^(#)?\$Env:WEBI_GIT_TAG\s*=.*/im,
-                "$Env:WEBI_GIT_TAG = '" + rel.git_tag + "'",
-              )
-              .replace(
-                /^(#)?\$Env:WEBI_PKG_URL\s*=.*/im,
-                "$Env:WEBI_PKG_URL = '" + rel.download + "'",
-              )
-              // TODO replace WEBI_PKG_FILE (which is sometimes a dir)
-              .replace(
-                /^(#)?\$Env:WEBI_PKG_PATHNAME\s*=.*/im,
-                "$Env:WEBI_PKG_PATHNAME = '" + rel.name + "'",
-              )
-              // TODO deprecate
-              .replace(
-                /^(#)?\$Env:WEBI_PKG_FILE\s*=.*/im,
-                "$Env:WEBI_PKG_FILE = '" + rel.name + "'",
-              )
-              .replace(reInstallTpl, '\n' + installTxt)
-          );
-        });
-    });
+  let installTxt = await fs.promises.readFile(
+    path.join(pkgdir, 'install.ps1'),
+    'utf8',
+  );
+  installTxt = padScript(installTxt);
+  /*
+    var vers = rel.version.split('.');
+    var v = {
+      major: vers.shift() || '',
+      minor: vers.shift() || '',
+      patch: vers.join('.').replace(/[+\-].*$/, ''),
+      build: vers
+        .join('.')
+        .replace(/[^+\-]*()/, '')
+        .replace(/^-/, '')
+    };
+  */
+  let tplTxt = await fs.promises.readFile(
+    path.join(__dirname, 'template.ps1'),
+    'utf8',
+  );
+  var pkgver = pkg + '@' + ver;
+  return (
+    tplTxt
+      .replace(
+        /^(#)?\$Env:WEBI_HOST\s*=.*/im,
+        "$Env:WEBI_HOST = '" + baseurl + "'",
+      )
+      .replace(
+        /^(#)?\$Env:WEBI_PKG\s*=.*/im,
+        "$Env:WEBI_PKG = '" + pkgver + "'",
+      )
+      .replace(/^(#)?\$Env:PKG_NAME\s*=.*/im, "$Env:PKG_NAME = '" + pkg + "'")
+      .replace(
+        /^(#)?\$Env:WEBI_VERSION\s*=.*/im,
+        "$Env:WEBI_VERSION = '" + rel.version + "'",
+      )
+      .replace(
+        /^(#)?\$Env:WEBI_GIT_TAG\s*=.*/im,
+        "$Env:WEBI_GIT_TAG = '" + rel.git_tag + "'",
+      )
+      .replace(
+        /^(#)?\$Env:WEBI_PKG_URL\s*=.*/im,
+        "$Env:WEBI_PKG_URL = '" + rel.download + "'",
+      )
+      // TODO replace WEBI_PKG_FILE (which is sometimes a dir)
+      .replace(
+        /^(#)?\$Env:WEBI_PKG_PATHNAME\s*=.*/im,
+        "$Env:WEBI_PKG_PATHNAME = '" + rel.name + "'",
+      )
+      // TODO deprecate
+      .replace(
+        /^(#)?\$Env:WEBI_PKG_FILE\s*=.*/im,
+        "$Env:WEBI_PKG_FILE = '" + rel.name + "'",
+      )
+      .replace(reInstallTpl, '\n' + installTxt)
+  );
 };
