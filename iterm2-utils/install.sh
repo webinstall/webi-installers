c6b37324a046e539cd8277ac0c7c78a6bc9e3c84
AJ ONeal <aj@therootcompany.com>
2023-11-07 09:28:50 +0000

    fix(iterm2-utils): add ~/.iterm2/ to PATH

--- iterm2-utils/install.sh
+++ iterm2-utils/install.sh
@@ -4,6 +4,8 @@ set -u
 
 __iterm2_utils() {
     curl -fsSL https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+
+    webi_path_add ~/.iterm2/
 }
 
 __iterm2_utils
