de36cf6b10755ab2126cd179280387214c66b9cc
AJ ONeal <aj@therootcompany.com>
2023-11-11 23:47:13 -0700

    ref!: delete alias 'pwsh' in prep for powershell => pwsh transition

--- pwsh/install.ps1
+++
@@ -1,8 +0,0 @@
-#!/bin/pwsh
-
-Write-Output "'pwsh@$Env:WEBI_TAG' is an alias for 'powershell@$Env:WEBI_VERSION'"
-
-$PwshBootUrl = "$Env:WEBI_HOST/powershell@$Env:WEBI_VERSION"
-$PwshBootScript = "$HOME\.local\tmp\install-pwsh-boot.ps1"
-Invoke-DownloadUrl -Force -URL $PwshBootUrl -Path $PwshBootScript
-powershell -ExecutionPolicy Bypass $PwshBootScript
