344c1d6af8ff061b48a2464b2db4e510e45f2943
AJ ONeal <aj@therootcompany.com>
2023-11-12 03:01:19 +0000

    fix(windows): make pwsh alias 'powershell ...' rather '... | powershell"

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1,5 +1,8 @@
 #!/bin/pwsh
 
 Write-Output "'pwsh@$Env:WEBI_TAG' is an alias for 'powershell@$Env:WEBI_VERSION'"
-IF ($null -eq $Env:WEBI_HOST -or $Env:WEBI_HOST -eq "") { $Env:WEBI_HOST = "https://webinstall.dev" }
-curl.exe -A MS -fsSL "$Env:WEBI_HOST/powershell@$Env:WEBI_VERSION" | powershell
+
+$PwshBootUrl = "$Env:WEBI_HOST/powershell@$Env:WEBI_VERSION"
+$PwshBootScript = "$HOME\.local\tmp\install-pwsh-boot.ps1"
+Invoke-DownloadUrl -Force -URL $PwshBootUrl -Path $PwshBootScript
+powershell -ExecutionPolicy Bypass $PwshBootScript
