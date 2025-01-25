2cf28cec60771a7f48f9c7784da5b856a9e11ff5
AJ ONeal <aj@therootcompany.com>
2025-01-25 00:01:41 +0000

    feat(mariadb): add mysql and mariadb-server aliases

---
+++ mariadbd/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+set -u
+
+__redirect_alias_mariadb() {
+    echo "'mariadbd' is an alias for 'mariadb'"
+    sleep 2.5
+    WEBI_HOST=${WEBI_HOST:-"https://webi.sh"}
+    curl -fsSL "$WEBI_HOST/mariadb@${WEBI_VERSION-}" | sh
+}
+
+__redirect_alias_mariadb
