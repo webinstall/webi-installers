cdc6f2e028713efd09367be9d32352f09ab7c2b0
Alexander Tebiev <alexander.tebiev@gmail.com>
2025-03-28 10:42:08 +0100

    add telegram-owl

---
+++ telegram-owl/releases.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let Releases = module.exports;
+
+let GitHub = require('../_common/github.js');
+let owner = 'beeyev';
+let repo = 'telegram-owl';
+
+Releases.latest = async function () {
+  let all = await GitHub.getDistributables(null, owner, repo);
+  return all;
+};
+
+if (module === require.main) {
+  (async function () {
+    let normalize = require('../_webi/normalize.js');
+    let all = await Releases.latest();
+    all = normalize(all);
+    // just select the first 5 for demonstration
+    all.releases = all.releases.slice(0, 5);
+    console.info(JSON.stringify(all, null, 2));
+  })();
+}
