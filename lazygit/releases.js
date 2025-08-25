cc8ed55d1f86a10cbf63f1e98c6c85b2eaf9726f
Chen Linxuan <me@black-desk.cn>
2025-08-25 20:37:58 +0800

    feat: add lazygit
    Signed-off-by: Chen Linxuan <me@black-desk.cn>

---
+++ lazygit/releases.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var github = require('../_common/github.js');
+var owner = 'jesseduffield';
+var repo = 'lazygit';
+
+module.exports = function () {
+  return github(null, owner, repo).then(function (all) {
+    return all;
+  });
+};
+
+if (module === require.main) {
+  module.exports().then(function (all) {
+    all = require('../_webi/normalize.js')(all);
+    // just select the first 5 for demonstration
+    all.releases = all.releases.slice(0, 5);
+    console.info(JSON.stringify(all, null, 2));
+  });
+}
