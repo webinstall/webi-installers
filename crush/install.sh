21ed58537d274d264edc6ed753d7a3117edcc184
ogormans-deptstack <sean.ogorman@deptagency.com>
2026-03-19 09:13:06 +0000

    fix(crush): use underscore format for binary subdirectory
    - Crush releases use underscore format: crush_VERSION_OS_arch/
    - Changed glob pattern from crush-*/ to crush_*/
    - Addresses feedback from @detox-24

--- crush/install.sh
+++ crush/install.sh
@@ -1,42 +1,42 @@
 #!/bin/sh
 
 __init_crush() {
-    set -e
-    set -u
+	set -e
+	set -u
 
-    ##################
-    # Install crush #
-    ##################
+	##################
+	# Install crush #
+	##################
 
-    # Every package should define these 6 variables
-    pkg_cmd_name="crush"
+	# Every package should define these 6 variables
+	pkg_cmd_name="crush"
 
-    pkg_dst_cmd="$HOME/.local/bin/crush"
-    pkg_dst="$pkg_dst_cmd"
+	pkg_dst_cmd="$HOME/.local/bin/crush"
+	pkg_dst="$pkg_dst_cmd"
 
-    pkg_src_cmd="$HOME/.local/opt/crush-v$WEBI_VERSION/bin/crush"
-    pkg_src_dir="$HOME/.local/opt/crush-v$WEBI_VERSION"
-    pkg_src="$pkg_src_cmd"
+	pkg_src_cmd="$HOME/.local/opt/crush-v$WEBI_VERSION/bin/crush"
+	pkg_src_dir="$HOME/.local/opt/crush-v$WEBI_VERSION"
+	pkg_src="$pkg_src_cmd"
 
-    pkg_install() {
-        # $HOME/.local/opt/crush-v0.50.1/bin
-        mkdir -p "$(dirname "$pkg_src_cmd")"
+	pkg_install() {
+		# $HOME/.local/opt/crush-v0.50.1/bin
+		mkdir -p "$(dirname "$pkg_src_cmd")"
 
-        # mv ./crush-*/* "$HOME/.local/opt/crush-v0.50.1/bin/"
-        # (goreleaser puts binaries in a subdirectory)
-        mv ./crush-*/"$pkg_cmd_name" "$pkg_src_cmd"
+		# mv ./crush_*/* "$HOME/.local/opt/crush-v0.50.1/bin/"
+		# (goreleaser puts binaries in a subdirectory with underscores: crush_VERSION_OS_arch/)
+		mv ./crush_*/"$pkg_cmd_name" "$pkg_src_cmd"
 
-        # chmod a+x "$HOME/.local/opt/crush-v0.50.1/bin/crush"
-        chmod a+x "$pkg_src_cmd"
-    }
+		# chmod a+x "$HOME/.local/opt/crush-v0.50.1/bin/crush"
+		chmod a+x "$pkg_src_cmd"
+	}
 
-    pkg_get_current_version() {
-        # 'crush --version' has output in this format:
-        #       0.50.1
-        # This trims it down to just the version number:
-        #       0.50.1
-        crush --version 2> /dev/null | head -n 1 | sed 's:^v::'
-    }
+	pkg_get_current_version() {
+		# 'crush --version' has output in this format:
+		#       0.50.1
+		# This trims it down to just the version number:
+		#       0.50.1
+		crush --version 2>/dev/null | head -n 1 | sed 's:^v::'
+	}
 
 }
 
