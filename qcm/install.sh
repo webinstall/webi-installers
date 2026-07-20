8629e9e93257ce8ec923f473e1adc640058ab8bc
Youg-Otricked <yougotricked2@gmail.com>
2026-07-19 21:00:00 -0700

    Add qcm installer

---
+++ qcm/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+set -u
+
+__init_qcm() {
+    WEBI_SINGLE=true
+
+    pkg_get_current_version() {
+        qcm --version 2>/dev/null | head -n 1 | cut -d ' ' -f 2
+    }
+    pkg_post_install() {
+        webi_post_install
+        "$pkg_dst_cmd" setup --no-export
+    }
+}
+
+__init_qcm
+
